a070c78e486eb59d4034cc7a0884c2078b510a40
sun <echo_eric@yeah.net>
2015-09-25 15:16:27 +0800

    Delete redundant functions
    delete some files

--- src/Monitors/tHttpd/src/version.h
+++
@@ -1,9 +0,0 @@
-/* version.h - version defines for thttpd and libhttpd */
-
-#ifndef _VERSION_H_
-#define _VERSION_H_
-
-#define SERVER_SOFTWARE "thttpd/2.26 14aug2014"
-#define SERVER_ADDRESS "http://www.acme.com/software/thttpd/"
-
-#endif /* _VERSION_H_ */
